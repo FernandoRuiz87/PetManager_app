e06db8a4c8cb0fb7dcd697f83ce48489aace3a27
FernandoRuiz87 <fruizjimenenez@gmail.com>
2025-05-03 07:53:21 -0600

    Refactorizar widgets: mejorar la estructura de CustomButton y agregar CustomTextButton con estilos personalizados.

--- lib/widgets/common_widgets.dart
+++ lib/widgets/common_widgets.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/material.dart';
 import 'package:pet_manager_app/colors/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'dart:io';
 
 // Header para el login y la pagina de registro
 class Header extends StatelessWidget {
@@ -12,41 +13,32 @@ class Header extends StatelessWidget {
     return Column(
       children: [
         Center(
-          child: Hero(
-            tag: 'logo_hero',
-            child: Image.asset(
-              "assets/images/logo.png",
-              width: logoSize,
-              height: logoSize,
-            ),
+          child: Image.asset(
+            "assets/images/logo.png",
+            width: logoSize,
+            height: logoSize,
           ),
         ),
         const SizedBox(height: 5),
         const Center(
-          child: Hero(
-            tag: 'app_name_hero',
-            child: Material(
-              color: Colors.transparent,
-              child: Text(
-                'PetManager',
-                style: TextStyle(
-                  fontSize: 36,
-                  fontWeight: FontWeight.bold,
-                  color: AppColors.textPrimary,
-                ),
+          child: Material(
+            color: Colors.transparent,
+            child: Text(
+              'PetManager',
+              style: TextStyle(
+                fontSize: 36,
+                fontWeight: FontWeight.bold,
+                color: AppColors.textPrimary,
               ),
             ),
           ),
         ),
         const SizedBox(height: 8),
-        Hero(
-          tag: 'subtitle_hero',
-          child: Material(
-            color: Colors.transparent,
-            child: const Text(
-              'Tu asistente para el cuidado de mascotas',
-              style: TextStyle(fontSize: 18, color: AppColors.textTertiary),
-            ),
+        Material(
+          color: Colors.transparent,
+          child: const Text(
+            'Tu asistente para el cuidado de mascotas',
+            style: TextStyle(fontSize: 18, color: AppColors.textTertiary),
           ),
         ),
       ],
@@ -76,6 +68,67 @@ class SectionDivider extends StatelessWidget {
           child: Divider(color: AppColors.textFieldBorderColor, thickness: 1.5),
         ),
       ],
+    );
+  }
+}
+
+class PetPicture extends StatelessWidget {
+  const PetPicture({super.key, required this.size, this.imagePath});
+
+  final double size;
+  final String? imagePath;
+
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(
+      radius: size,
+      backgroundColor: AppColors.background,
+      child: ClipOval(
+        child: SizedBox(
+          width: size * 2,
+          height: size * 2,
+          child:
+              imagePath != null
+                  ? Image.file(
+                    File(imagePath!),
+                    fit: BoxFit.cover,
+                    errorBuilder:
+                        (_, __, ___) => Container(color: AppColors.secondary),
+                  )
+                  : Image.asset(
+                    "assets/images/default_pet.png",
+                    fit: BoxFit.cover,
+                    errorBuilder:
+                        (_, __, ___) => Container(color: AppColors.secondary),
+                  ),
+        ),
+      ),
+    );
+  }
+}
+
+class NavBar extends StatelessWidget {
+  final int currentIndex;
+
+  const NavBar({super.key, required this.currentIndex});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Inicio'),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.calendar_month),
+          label: 'Calendario',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.settings),
+          label: 'Configuración',
+        ),
+      ],
+      currentIndex: currentIndex,
+      selectedItemColor: AppColors.primary,
+      onTap: (index) {},
     );
   }
 }
--- lib/widgets/custom_buttons.dart
+++ lib/widgets/custom_buttons.dart
@@ -5,43 +5,53 @@ import 'package:pet_manager_app/colors/app_colors.dart';
 class CustomButton extends StatelessWidget {
   const CustomButton({
     super.key,
-    required this.buttonColor,
     required this.text,
+    required this.buttonColor,
     required this.foregroundColor,
     required this.onPressed,
+    required this.height,
   });
 
   final Color buttonColor;
   final String text;
   final Color foregroundColor;
   final VoidCallback onPressed; // Callback para la accion del boton
+  final double height;
 
   @override
   Widget build(BuildContext context) {
-    return OutlinedButton(
-      onPressed: onPressed,
-      style: OutlinedButton.styleFrom(
-        padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
-        textStyle: const TextStyle(fontSize: 18),
-        backgroundColor: buttonColor,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
-        side: BorderSide(color: buttonColor),
+    return SizedBox(
+      height: height,
+      child: OutlinedButton(
+        onPressed: onPressed,
+        style: OutlinedButton.styleFrom(
+          textStyle: const TextStyle(fontSize: 18),
+          backgroundColor: buttonColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(50),
+          ),
+          side: BorderSide(color: buttonColor),
+        ),
+        child: Text(
+          text,
+          style: TextStyle(fontSize: 16, color: foregroundColor),
+        ),
       ),
-      child: Text(text, style: TextStyle(fontSize: 16, color: foregroundColor)),
     );
   }
 }
 
 // Boton de agregar
 class AddButton extends StatelessWidget {
-  const AddButton({super.key});
+  const AddButton({super.key, required this.onPressed});
+  final VoidCallback onPressed; // Callback para la accion del boton
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 30,
       child: OutlinedButton(
-        onPressed: () {},
+        onPressed: onPressed,
         style: OutlinedButton.styleFrom(
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 0),
           backgroundColor: AppColors.primary,
@@ -62,6 +72,38 @@ class AddButton extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+}
+
+class CustomTextButton extends StatelessWidget {
+  const CustomTextButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+    required this.textColor,
+    required this.fontSize,
+  });
+
+  final String text;
+  final VoidCallback onPressed;
+  final Color textColor;
+  final double fontSize;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: onPressed,
+      style: TextButton.styleFrom(
+        foregroundColor: textColor,
+        textStyle: TextStyle(
+          fontSize: fontSize,
+          fontWeight: FontWeight.w500,
+          decoration: TextDecoration.underline,
+          decorationColor: textColor,
+        ),
+      ),
+      child: Text(text),
     );
   }
 }
